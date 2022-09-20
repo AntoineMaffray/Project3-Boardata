bfb8ef64b064927ecbebdf09e757c90d8ab55ebc
Antoine Maffay <amaffray1@gmail.com>
2022-09-20 10:37:17 +0200

    toudébut

---
+++ boardata/src/main/java/com/eql/project3/boardata/BoardataApplication.java
@@ -0,0 +1,13 @@
+package com.eql.project3.boardata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardataApplication.class, args);
+	}
+
+}
---
+++ boardata/src/test/java/com/eql/project3/boardata/BoardataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eql.project3.boardata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
